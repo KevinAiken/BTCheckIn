2cd5e0f8077c988f0251106cf95c55d5a54aecf5
Nishant Sinha <nishnha@gmail.com>
2016-10-23 05:46:47 -0400

    fixed website

--- index.php
+++ index.php
@@ -1,23 +1,17 @@
 <!DOCTYPE html>
 <html>
-    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"
-  integrity="sha256-/SIrNqv8h6QGKDuNoLGA4iret+kyesCkHGzVUUV0shc="
-  crossorigin="anonymous"></script>
+    <script src="js/jquery-3.1.1.min.js"</script>
     <script type="text/javascript" src="js/handlebars-v4.0.5.js"></script>
 
 <head>
     <meta charset="utf-8">
-    <title>Employee Times</title>
+    <title>Employee Checkin</title>
     <link  type="text/css" rel=stylesheet href="css/main.css">
 </head>
 
 <body>
     <sidebar>
         <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
             <li><a href="http://github.com/KevinAiken/BTCheckIn">Github</a></li>
         </ul>
     </sidebar>
@@ -33,11 +27,8 @@
                         <div class="name">
                             <h2>{{name}}</h2>
                         </div>
-                        <div class="isIn">
-                            <p>{{isIn}}</p>
-                        </div>
-                        <div class="lastIn">
-                            <p>{{lastIn}}</p>
+                        <div class="status">
+                            <p>{{status}}</p>
                         </div>
                     </div>
                 </div>
@@ -46,26 +37,37 @@
     </div>
 </body>
 
-
 <script src="js/cards.js"></script>
+
+<script>
 <?php
-$username = "monitor";
-$password = "MichaelChung1!";
-$hostname = "localhost";
-//php connect to mysql
-$conn = mysql_connect($hostname, $username, $password);
+$servername = 'localhost';
+$username = 'monitor';
+$password = 'MichaelChung1!';
 
-mysql_select_db("users");
-$result = mysql_query("SELECT * FROM usersdat", $conn);
-if(!$result){
-  die("Could not get data: ". mysql_error());
-}
-$scriptString = "<script type=\"text/javascript\">";
-while($row = mysql_fetch_array($result)){
-  $scriptString += "card(\"res/avatar.jpg\","+ $row[1] + ", \""+ $row[2]+ "," + $row[3] ");";
-}
-$scriptString += "</script>";
+$conn = mysql_connect($servername, $username, $password);
+if( ! $conn) {
+	die('Could not connect: ' . mysql_error());
+} 
 
-echo $scriptString;
+$sql = 'SELECT * FROM userstatus';
+mysql_select_db('users');
+$retval = mysql_query($sql, $conn);
+
+if(!$retval){
+	die('Could not get data: ' . mysql_error());
+}
+
+$cards = "";
+
+while($row = mysql_fetch_assoc($retval)){
+	cards += 'card(\"res/{$row['name']}.png\", \"' + {$row['name']} + '\", \" + {row['status']} + '\");<br>';
+}
+
+echo $cards;
+
+mysql_close($conn);
 ?>
+</script>
+    
 </html>
--- js/cards.js
+++ js/cards.js
@@ -1,10 +1,16 @@
 var source   = $("#card-template").html();
 var template = Handlebars.compile(source);
 
-function card(img, name, front, back) {
-    var context  = {img: img, name: name, isIn: name + " last checked in at " + front, lastIn: "Last checked out at " + back};
+function card(img, name, value) {
+    var status;
+    
+    if (value == 0) {
+        status = name + " is not here right now";
+        $(".card").addClass("darken");
+    } else {
+        status = name + " is present";
+    }
+    
+    var context = {img: img, name: name, status: status};
     $("#center-cards").append(template(context));
 }
-
-card("res/KevinAiken.png", "Kevin Aiken", "9:05", "4:20");
-card("res/avatar.jpg", "Nishant Sinha", "6:30", "8:20");
